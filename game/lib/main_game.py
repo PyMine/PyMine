79c4052a7a943fa3b9ec5311ad60c9aa56908f06
Red_M <pooatyou.com@gmail.com>
2014-09-24 02:41:09 +1000

    Then i found a better way of calling other game functions from the main core game file.

--- game/lib/main_game.py
+++ game/lib/main_game.py
@@ -1,3 +1,5 @@
-def main(test_game_core):
+from lib import test_game_core
+
+def main():
     print("this is from the maingame file")
     test_game_core.main()
--- launcher.py
+++ launcher.py
@@ -13,7 +13,6 @@ game_path = os.path.join(current_dir,"game")
 sys.path += ['game']
 os.chdir(current_dir)
 from lib import main_game
-from lib import test_game_core
 
 print("this is from the launcher")
-main_game.main(test_game_core)
+main_game.main()
