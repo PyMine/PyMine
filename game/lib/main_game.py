dc350bfd80fd3db488bdc75163d868dc5cecd44a
Red_M <pooatyou.com@gmail.com>
2014-09-24 06:43:29 +1000

    played with the launcher a little bit and added an icon for the game made by MaxwellEdison.

--- game/lib/main_game.py
+++ game/lib/main_game.py
@@ -10,22 +10,23 @@ class game(object):
 game.keys_pressed = {
 "left" : False,
 "right" : False,
-"forwards" : False,
-"backwards" : False
+"forward" : False,
+"backward" : False
 }
 game.config_keymap = {
 "a" : "left",
 "d" : "right",
-"w" : "forwards",
-"s" : "backwards"
+"w" : "forward",
+"s" : "backward"
 }
 
-def main():
+def main(launcher_vars):
     
     pygame.init()
     screen = pygame.display.set_mode([800, 600], pygame.OPENGL | pygame.DOUBLEBUF)
     print(pygame.display.Info())
-    #pygame.display.set_icon() #sets the icon for the game
+    icon_path = os.path.join(launcher_vars["texture"],"icon.ico")
+    pygame.display.set_icon(icon_path) #sets the icon for the game
     pygame.display.set_caption("PyMine")
     while 1:
         for event in pygame.event.get():
--- launcher.py
+++ launcher.py
@@ -9,10 +9,21 @@ import time
 global current_dir
 current_dir = os.path.abspath('')
 game_path = os.path.join(current_dir,"game")
+texture_path = os.path.join(game_path,"textures")
+save_path = os.path.join(game_path,"saves")
+mod_path = os.path.join(game_path,"mods")
+config_path = os.path.join(game_path,"config.conf")
 
 sys.path += ['game']
 os.chdir(current_dir)
 from lib import main_game
 
 print("this is from the launcher")
-main_game.main()
+launcher_vars = {
+"game" : game_path,
+"texture" : texture_path,
+"save" : save_path,
+"mod" : mod_path,
+"config" : config_path
+}
+main_game.main(launcher_vars)
