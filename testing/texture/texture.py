e6d26d2c8863a124afec907f0c4df824e7c8eb8a
Christopher <ggmethos@autistici.org>
2014-10-11 14:54:23 -0400

    fixed it

--- testing/texture/texture.py
+++ testing/texture/texture.py
@@ -1,20 +1,34 @@
-#! /usr/bin/env python
-from OpenGL import GL
-from OpenGL.GL.shaders import compileProgram,compileShader
-import pygame as pg,sys,os
-from PIL.Image import open
-except ImportError, err:
-from Image import open
-#BaseContext = testingcontext.getInteractive()
+from OpenGLContext import testingcontext
+BaseContext = testingcontext.getInteractive()
 from OpenGL.GL import *
 import time
 try:
+    from PIL.Image import open
+except ImportError, err:
+    from Image import open
 class TestContext( BaseContext ):
-    """Demo"""
+    """Maxie Texture Demo"""
     initialPosition = (0,0,0) # set initial camera position, tutorial does the re-positioning
+
+class TestContext( BaseContext ):
+    """Maxie Texture Demo"""
+    initialPosition = (0,0,0) # set initial camera position; re-position happens by itself
     def OnInit( self ):
         """Load the image on initial load of the application"""
         self.imageID = self.loadImage ()
+
+    #def loadTexture(imageName= "1.bmp"):
+    #    img = PIL.Image.open(name)
+    #    img_data = numpy.array(list(img.getdata()), numpy.int8)
+    #    id = glGenTextures(1)
+    #    glPixelStorei(GL_UNPACK_ALIGNMENT,1)
+    #    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
+    #    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
+    #    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    #    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    #    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.size[0], img.size[1], 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
+    #    return id
+
     def loadImage( self, imageName = "1.bmp" ):
         """Load an image file as a 2D texture using PIL"""
         im = open(imageName)
@@ -23,7 +37,7 @@ class TestContext( BaseContext ):
         except SystemError:
             ix, iy, image = im.size[0], im.size[1], im.tostring("raw", "RGBX", 0, -1)
         ID = glGenTextures(1)
-      	glBindTexture(GL_TEXTURE_2D, ID)
+        glBindTexture(GL_TEXTURE_2D, ID)
         glPixelStorei(GL_UNPACK_ALIGNMENT,1)
         glTexImage2D(
         GL_TEXTURE_2D, 0, 3, ix, iy, 0,
@@ -31,51 +45,48 @@ class TestContext( BaseContext ):
         )
         return ID
     def Render( self, mode):
-        """Render scene geometry"""
+        """Render"""
         BaseContext.Render( self, mode )
         glDisable( GL_LIGHTING) # context lights by default
         glTranslatef(1.5,0.0,-6.0);
         glRotated( time.time()%(8.0)/8 * -360, 1,0,0)
         self.setupTexture()
         self.drawCube()
-            
     def setupTexture( self ):
         """For rendering"""
         glEnable(GL_TEXTURE_2D)
-    	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-    	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
         glBindTexture(GL_TEXTURE_2D, self.imageID)
-
     def drawCube( self ):
         """Draw a cube with Max's texture coordinates"""
-      	glBegin(GL_QUADS);
-       	glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
-    	glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
-      	glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0,  1.0);
-      	glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0,  1.0);
-      	glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0, -1.0);
-      	glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
-      	glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
-      	glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0, -1.0);
-      	glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
-      	glTexCoord2f(0.0, 0.0); glVertex3f(-1.0,  1.0,  1.0);
-      	glTexCoord2f(1.0, 0.0); glVertex3f( 1.0,  1.0,  1.0);
-      	glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
-      	glTexCoord2f(1.0, 1.0); glVertex3f(-1.0, -1.0, -1.0);
-      	glTexCoord2f(0.0, 1.0); glVertex3f( 1.0, -1.0, -1.0);
-      	glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
-      	glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
-      	glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0, -1.0);
-      	glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
-      	glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0,  1.0);
-      	glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
-      	glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0, -1.0);
-      	glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
-      	glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0,  1.0);
-      	glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
-      	glEnd()
-    
+        glBegin(GL_QUADS);
+        glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
+        glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
+        glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0,  1.0);
+        glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0,  1.0);
+        glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0, -1.0);
+        glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
+        glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
+        glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0, -1.0);
+        glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
+        glTexCoord2f(0.0, 0.0); glVertex3f(-1.0,  1.0,  1.0);
+        glTexCoord2f(1.0, 0.0); glVertex3f( 1.0,  1.0,  1.0);
+        glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
+        glTexCoord2f(1.0, 1.0); glVertex3f(-1.0, -1.0, -1.0);
+        glTexCoord2f(0.0, 1.0); glVertex3f( 1.0, -1.0, -1.0);
+        glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
+        glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
+        glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0, -1.0);
+        glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
+        glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0,  1.0);
+        glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
+        glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0, -1.0);
+        glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
+        glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0,  1.0);
+        glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
+        glEnd()
     def OnIdle( self, ):
         """Request refresh of the context whenever idle"""
         self.triggerRedraw(1)
